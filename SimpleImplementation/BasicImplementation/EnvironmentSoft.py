929d64174148bd39eb84c1d1df7c955adb440e7f
UncleBen420 <remyvu@outlook.com>
2022-10-04 13:06:14 +0200

    minor change

--- SimpleImplementation/BasicImplementation/EnvironmentSoft.py
+++ SimpleImplementation/BasicImplementation/EnvironmentSoft.py
@@ -290,7 +290,8 @@ class SoftEnv:
         if action == Action.MARK and not self.marked[-1] in self.marked[:-1]:
             window = self.model_resolution ** self.z
             marked = self.bb_map[window * self.x:window + window * self.x, window * self.y:window + window * self.y]
-            reward += 100 if np.count_nonzero(marked) else -50
+            reward += np.count_nonzero(marked) * 10
+            reward -= marked.size - np.count_nonzero(marked)
         elif action == Action.MARK:
             reward -= 100
 
