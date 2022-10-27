c9c2b23a4c30eb9f9ee20f8979abeba9b85d9345
UncleBen420 <remyvu@outlook.com>
2022-10-27 16:10:49 +0200

    minor change

--- RealImplementation/reinforce.py
+++ RealImplementation/reinforce.py
@@ -231,10 +231,10 @@ class Reinforce:
                 # MODEL OPTIMISATION
                 # ------------------------------------------------------------------------------------------------------
                 mean_loss = 0.
-                if len(good_behaviour_dataset) > 10:
+                if len(good_behaviour_dataset) > 3:
                     dataset = random.choices(good_behaviour_dataset, k=3)
-                    dataset.append((0, (S_batch, A_batch, G_batch)))
                     mean_loss += self.update_policy(dataset)
+
 
                 # ------------------------------------------------------------------------------------------------------
                 # METRICS RECORD
